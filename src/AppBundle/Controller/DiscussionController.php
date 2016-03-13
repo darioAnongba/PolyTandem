774a5905971c7352f9acd009c0b0b88d5904594d
DarioAnongba <dario.anongba@epfl.ch>
2016-03-13 11:23:06 +0100

    Discussions view

---
+++ src/AppBundle/Controller/DiscussionController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\LearningLanguage;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/discussions")
+ */
+class DiscussionController extends Controller
+{
+    /**
+     * @Route("/", name="discussions")
+     */
+    public function indexAction(Request $request)
+    {
+        $repo = $this->getDoctrine()->getManager()->getRepository('AppBundle:Discussion');
+        $discussions = $repo->findBy(['host' => $this->getUser()]);
+
+        return $this->render('discussions/index.html.twig', [
+            'discussions' => $discussions
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="deleteDiscussion")
+     */
+    public function deleteDiscussionAction($id)
+    {
+        $repo = $this->getDoctrine()->getManager()->getRepository('AppBundle:Discussion');
+        $discussion = $repo->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($discussion);
+        $em->flush();
+
+        $this->addFlash('notice', 'Discussion deleted');
+
+        return $this->redirectToRoute('discussions');
+    }
+}
